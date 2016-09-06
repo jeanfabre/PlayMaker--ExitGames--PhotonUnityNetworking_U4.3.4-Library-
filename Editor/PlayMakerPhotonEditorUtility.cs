4386531dcf7a558cf8d3de09f81a683bab00d391
Jean Fabre <contact@fabrejean.net>
2016-09-06 15:00:19 +0300

    updated support for 1.75

--- Editor/PlayMakerPhotonEditorUtility.cs
+++ Editor/PlayMakerPhotonEditorUtility.cs
@@ -17,7 +17,7 @@ public class PlayMakerPhotonEditorUtility : Editor
 {
     const string PlayMakerPhotonMenuRoot = "PlayMaker/Addons/Photon Networking/";
 	
-    public const float supportedPUNVersion = 1.74f;
+    public const float supportedPUNVersion = 1.75f;
 
 	static string PlayMakerPhotonProxyName = "PlayMaker Photon Proxy";
 	
