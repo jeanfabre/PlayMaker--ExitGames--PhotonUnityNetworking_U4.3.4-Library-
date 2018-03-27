4414a98139d7773935a5c2cf9c198e73008e1923
Jean Fabre <contact@fabrejean.net>
2018-03-27 14:59:43 +0300

    Preparing for PlayMaker 1.9 interface change

---
+++ Editor/PlayMakerPhotonEditorSetup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+using HutongGames.PlayMakerEditor;
+
+[InitializeOnLoad]
+public class PlayMakerPhotonEditorSetup
+{
+	static PlayMakerPhotonEditorSetup()
+	{
+		#if PLAYMAKER_1_9_OR_NEWER
+			FsmEditorSettings.ShowNetworkSync = true;
+		#endif
+	}
+
+}
